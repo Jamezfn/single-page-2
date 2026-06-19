c9605b228b396600a72691486b957cfa92af020c
Jamezfn <jamesmakachia19@gmail.com>
2026-06-19 10:53:56 +0300

    Implemented the featured project

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,12 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import Image from "next/image";
+import CheckIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 const portfolioProjects = [
   {
@@ -8,9 +14,9 @@ const portfolioProjects = [
     year: "2022",
     title: "Dark Saas Landing Page",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+		{ title: "Enhanced user experience by 40%" },
+		{ title: "Improved site speed by 50%" },
+		{ title: "Increased mobile traffic by 35%" },
     ],
     link: "https://youtu.be/4k7IdSLxh6w",
     image: darkSaasLandingPage,
@@ -20,9 +26,9 @@ const portfolioProjects = [
     year: "2021",
     title: "Light Saas Landing Page",
     results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
+		{ title: "Boosted sales by 20%" },
+		{ title: "Expanded customer reach by 35%" },
+		{ title: "Increased brand awareness by 15%" },
     ],
     link: "https://youtu.be/7hi5zwO75yc",
     image: lightSaasLandingPage,
@@ -32,9 +38,9 @@ const portfolioProjects = [
     year: "2023",
     title: "AI Startup Landing Page",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+		{ title: "Enhanced user experience by 40%" },
+		{ title: "Improved site speed by 50%" },
+		{ title: "Increased mobile traffic by 35%" },
     ],
     link: "https://youtu.be/Z7I5uSRHMHg",
     image: aiStartupLandingPage,
@@ -42,5 +48,60 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+ 	return (
+		<section className="pb-16 lg:py-24">
+			<div className='container'>
+				<div className="flex justify-center">
+					<p className="uppercase font-semibold tracking-widest bg-linear-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text">Real-world Results</p>
+				</div>
+				<h2 className="font-serif text-3xl md:text-5xl text-center mt-6">Featured Projects</h2>
+				<p className="text-center md:text-lg lg:text-xl text-foreground/60 mt-4 max-w-md mx-auto">See how I transformed concepts into engaging digital experiences.</p>
+				<div className="flex flex-col mt-10 md:mt-20 gap-20">
+					{portfolioProjects.map(project => (
+						<div
+							key={project.title} 
+							className="bg-gray-800 rounded-3xl relative z-0
+							overflow-hidden after:content-[''] after:absolute after:z-10 after:inset-0 
+							after:outline after:-outline-offset-2 after:rounded-3xl after:outline-foreground/20 px-8 pt-8 lg:pt-16 md:pt-12 md:px-10 lg:px-20
+							after:pointer-events-none"
+							>
+							<div className="absolute inset-0 -z-10 opacity-5" style={{ backgroundImage: `url(${grainImage.src})` }}></div>
+							<div className="lg:grid lg:grid-cols-2 lg:gap-16">
+								<div className="lg:pb-16">
+									<div className="inline-flex gap-2 bg-linear-to-r from-emerald-300 to-sky-400 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+										<span>{project.company}</span>
+										<span>&bull;</span>
+										<span>{project.year}</span>
+									</div>
+									<h3 className="font-serif text-2xl md:text-4xl mt-2 md:mt-5">{project.title}</h3>
+									<hr className="border-t-2 border-foreground/5 mt-4 md:mt-5" />
+									<ul className="flex flex-col gap-4 mt-4 md:mt-5">
+										{project.results.map(result => (
+											<li key={result.title} className="flex gap-2 text-sm md:text-base text-foreground/50">
+												<CheckIcon className="size-5 md:size-6"/>
+												<span>{result.title}</span>
+											</li>
+										))}
+									</ul>
+									<a href={project.link}>
+										<button className="inline-flex items-center justify-center gap-2 mt-8 bg-foreground text-background h-12 w-full md:w-auto md:px-6 rounded-xl font-semibold">
+											<span>Visit Live Site</span>
+											<FontAwesomeIcon icon={faArrowUp} className="flex items-center size-4 rotate-45" />
+										</button>
+									</a>
+								</div>
+								<div className="relative">
+									<Image 
+										src={project.image} 
+										alt={project.title} 
+										className="mt-8 lg:mt-0 -mb-4 md:mb-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+									/>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</section>
+	);
 };

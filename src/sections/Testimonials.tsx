9676ddbffb5281e3dced0995b883d88ed6f16146
Jamezfn <jamesmakachia19@gmail.com>
2026-06-20 14:07:14 +0300

    Implemented  the Testimonials section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -53,7 +53,7 @@ export const TestimonialsSection = () => {
 				<div className="mt-16 lg:mt-24 flex overflow-x-clip mask-[linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
 					<div className="flex gap-8 items-start flex-none">
 						{testimonials.map(testimonial => (
-							<Card key={testimonial.name} className="max-w-xs md:max-w-md md:p-8" >
+							<Card key={testimonial.name} className="max-w-xs md:max-w-md md:p-8 lg:pb-14" >
 								<div className="flex gap-4 items-center">
 									<div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full shrink-0">
 										<Image src={testimonial.avatar} alt={testimonial.name} className="max-h-full" />
